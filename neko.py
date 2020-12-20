db3f3ae70ffc077d6438d59ec9f69df9b7aa62e9
alectramell <alectramell@gmail.com>
2020-12-19 17:30:27 -0800

    neko

--- neko.py
+++ neko.py
@@ -1,6 +1,13 @@
 import os
 import sys
+import time
 
+os.system('cls')
+os.remove('notes.html')
+os.system('cls')
+time.sleep(0.5)
+os.system('cls')
+os.system('wget https://raw.githubusercontent.com/alectramell/neko/master/notes.html')
 os.system('cls')
 os.system('START /min neko.hta')
 os.system('cls')
